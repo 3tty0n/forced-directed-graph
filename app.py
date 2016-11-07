8e4e84c55d2196bc539bc7aefb4fa4cf9a0eb623
YuIzawa <yuizalp@gmail.com>
2016-11-08 01:46:27 +0900

    add project

---
+++ app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, url_for, redirect
+from utils.util import create_json_from_file
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+def generate_json(name):
+    return {"name": name, "url": '/view/' + name, "generate_url": '/api/' + name}
+
+graph = list()
+graph.append(generate_json('twitter_combined'))
+graph.append(generate_json('com-amazon.ungraph'))
+graph.append(generate_json('BA10000'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           title='Forced-Directed Layout',
+                           subtitle='built with flask, NetworkX and D3.js.',
+                           github='https://github.com/3tty0n/graph_sampling',
+                           graphs=graph)
+
+
+@app.route('/view/<filename>')
+def render(filename):
+    js_url = url_for('static', filename='forced.js')
+    json_url = url_for('static', filename=filename + '.json')
+    return render_template('forced_graph.html',
+                           title=filename,
+                           js_url=js_url,
+                           graph_name=json_url)
+
+
+@app.route('/api/<filename>')
+def generate_graph(filename):
+    js_url = url_for('static', filename='forced.js')
+    json_url = url_for('static', filename=filename + '.json')
+
+    if filename == 'twitter_combined' or filename == 'com-amazon.ungraph':
+        create_json_from_file(filename, 3000)
+    else:
+        create_json_from_file(filename, 1000)
+
+    return render_template('forced_graph.html', title=filename, js_url=js_url, graph_name=json_url)
+
+
+if __name__ == '__main__':
+    print('\nGo to http://localhost:5000 to see the example\n')
+    app.run()
---
+++ utils/util.py
@@ -0,0 +1,52 @@
+# coding=utf-8
+
+from typing import *
+
+import itertools
+import json
+import networkx as nx
+from networkx.readwrite import json_graph
+import numpy as np
+import random
+
+
+def random_walk(graph, start_node=None, size=-1, metropolized=False) -> List[int]:
+    """
+    RWでサンプリングしたノード列を返す
+
+    :param graph: グラフ
+    :param start_node: 先頭ノード
+    :param size: ノード列のサイズ
+    :param metropolized: metropolis hasting random walk フラグ
+    :return: サンプリングしたノード列
+    """
+    if start_node is None:
+        start_node = random.choice(graph.nodes())
+
+    v = start_node
+    for c in itertools.count():
+        if c == size:
+            return
+        if metropolized:
+            candidate = random.choice(graph.neighbors(v))
+            v = candidate if (random.random() < float(graph.degree(v)) / graph.degree(candidate)) else v
+        else:
+            v = random.choice(graph.neighbors(v))
+
+        yield v
+
+
+def create_json_from_file(file, size):
+    """
+    ファイルを読み込んでjsonを生成する
+    :param file: ファイル名
+    """
+    G = nx.read_edgelist('data/' + file + '.txt')
+    edges = list(random_walk(graph=G, size=size, metropolized=False))
+    G1 = nx.Graph()
+    G1.add_path(edges)
+    for n in G1:
+        G1.node[n]['name'] = n
+    d = json_graph.node_link_data(G1)
+    json.dump(d, open('static/' + file + '.json', 'w'))
+    print('Wrote node-link JSON data to static/' + file + '.json')
